bfb2f09a56a9049cdd529a03339ff37faaf3511a
KostaUrumov <koceurumov@gmail.com>
2023-04-12 15:28:20 +0000

    new SportsData

---
+++ SportsData/Controllers/StadiumController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsData.Controllers
+{
+    public class StadiumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddStadium()
+        {
+            return View();
+        }
+    }
+}
---
+++ SportsData/Models/AddStadiumModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsData.Models
+{
+    public class AddStadiumModel
+    {
+        public int StadiumId { get; set; }
+
+        [Required]
+        public string StadiumName { get; set; } = null!;
+        [Required]
+        public int Capacity { get; set; }
+    }
+}
