73381966278af9c6264e5fee55bb1264afc4f1e5
Kostadin Urumov <koceurumov@gmail.com>
2023-04-20 13:06:16 +0300

    ggg

--- SportsData/Models/AddCoachModel.cs
+++ SportsData/Models/AddCoachModel.cs
@@ -10,13 +10,22 @@ namespace SportsData.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Първо име")]
+
+        [MaxLength(GlobalConstraints.CoachNameMax)]
+        [MinLength(GlobalConstraints.CoachNameMin)]
         public string FirtsName { get; set; } = null!;
+
+
         [Required]
         [DisplayName("Фамилно име")]
+        [MaxLength(GlobalConstraints.CoachNameMax)]
+        [MinLength(GlobalConstraints.CoachNameMin)]
+
         public string LastName { get; set; } = null!;
 
         [Required]
         [DisplayName("Възраст")]
+        [Range(GlobalConstraints.CoachAgeMin, GlobalConstraints.CoachAgeMax)]
         public int Age { get; set; }
 
 
--- SportsData/Models/AddStadiumModel.cs
+++ SportsData/Models/AddStadiumModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using SportsData.Constraints;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SportsData.Models
@@ -7,9 +8,12 @@ namespace SportsData.Models
     {
         [Required]
         [DisplayName("Име")]
+        [MaxLength(GlobalConstraints.StadiumNameMax)]
         public string StadiumName { get; set; } = null!;
+
         [Required]
         [DisplayName("Капацитет")]
+        [Range(1, GlobalConstraints.StadiumCapacityMax)]
         public int Capacity { get; set; }
     }
 }
